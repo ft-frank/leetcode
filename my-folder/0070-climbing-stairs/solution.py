1eba67696663590da2c3b714d1dc02d911c51706
Frank Tran <166266231+ft-frank@users.noreply.github.com>
2026-07-27 05:31:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (98.01%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,19 +1,17 @@
-import math
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def factorial(x):
-            return math.factorial(x)
         
-        def combination(n, k):
-            return factorial(n) // (factorial(k) * factorial(n - k))
-        
-        ways = 0
-        max_two_steps = n // 2
-        
-        for two_steps in range(max_two_steps + 1):
-            one_steps = n - 2 * two_steps
-            total_steps = one_steps + two_steps
-            ways += combination(total_steps, two_steps)
-        
-        return ways
+        memo = {1: 1, 2:2}
+
+        def climb(n):
+            if n in memo:
+                return memo[n]
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            res = climb(n-1)+ climb(n-2)
+
+            memo[n] = res
+            return res
+        return climb(n)
